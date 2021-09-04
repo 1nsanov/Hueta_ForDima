f7a8f4f6964902b216b3255b984126894d61cbb5
1nsanov <73361511+1nsanov@users.noreply.github.com>
2021-09-04 23:34:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppHuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            var users = new List<User>();
+            for (int i = 1; i < 3; i++)
+            {
+                Console.Write("Введите имя: ");
+                var name = InputString();
+                Console.Write("Введите возраст: ");
+                var age = IntParse();
+                Console.Write("Введите пол: ");
+                var gender = InputString();
+
+                var user = new User(name, age, gender);                
+                users.Add(user);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var item in users)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        static private string InputString()
+        {
+            while (true)
+            {
+                var name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Строка не может быть пустой.");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    return name; 
+                }
+            }            
+        }
+
+        static private int IntParse()
+        {
+            while (true)
+            {
+                var age = Console.ReadLine();
+                if (int.TryParse(age, out int a))
+                {
+                    return a;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ошибка ввода.");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+            }           
+        }
+        
+    }
+}
---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHuy
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public User(string name, int age, string gender)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(gender))
+            {
+                throw new ArgumentNullException(nameof(gender));
+            }
+
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} | {Age} | {Gender}"; 
+        }
+    }
+}
